b2d960350065177f12c518420a5a2b8346956536
AndrewL123456 <andrewlujan763@gmail.com>
2022-01-31 14:08:43 -0700

    Finished project

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
+    @IBOutlet weak var numberText: UITextField!
+    
+    @IBOutlet weak var mySecondImage: UIImageView!
+    
+    @IBOutlet weak var bandLabel: UILabel!
+    
+    @IBOutlet weak var bandMembers: UILabel!
+    
+    
+    let favoriteBand = "Muse"
+    let numberOFMembers = "4"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myImage.image = UIImage(named: "Mountains1")
+        mySecondImage.image = UIImage(named: "capybara-square-1")
+        bandLabel.text = "\(favoriteBand)"
+        bandMembers.text = "There are \(numberOFMembers) members in Muse"
     }
 
-
+    @IBAction func textClear(_ sender: Any) {
+        numberText.text = ""
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
 }
 
